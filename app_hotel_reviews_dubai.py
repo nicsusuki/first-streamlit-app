e8319ac6351e90b5fe2ee47f862453fe1fe41d22
nicsusuki <118863992+nicsusuki@users.noreply.github.com>
2022-11-29 14:18:11 -0600

    Update app_hotel_reviews_dubai.py

--- app_hotel_reviews_dubai.py
+++ app_hotel_reviews_dubai.py
@@ -16,73 +16,64 @@ from tqdm import tqdm
 
 
 
-st.title("Nicole Susuki")
-st.markdown("This is a demo Streamlit app.")
-st.markdown("My name is Hamza, hello world!..")
-st.markdown("This is v2")
+st.title("Dubai Hotel Finder")
+st.subheader("Find a hotel in Dubai via review similarity")
+queries = st.text_input("Start typing")
+# =============================================================================
+# st.markdown("This is a demo Streamlit app.")
+# st.markdown("My name is Hamza, hello world!..")
+# st.markdown("This is v2")
+# =============================================================================
 
 @st.cache(persist=True)
-def load_data():
-    url = 'https://raw.githubusercontent.com/nicsusuki/first-streamlit-app/main/hotelReviewsInDubai__en2019100120191005.csv'
-    df = pd.read_csv(url)
-    
-    df['hotelName'] = df.hotelName.str.split('\n').str[0]
-    df['hotelName'] = df.hotelName.str.split('   ').str[1]
-    df['hotelName'].drop_duplicates()
-    df_combined = df.sort_values(['hotelName']).groupby('hotelName', sort=False).review_body.apply(''.join).reset_index(name='all_review')
-      
-    df_combined['all_review'] = df_combined['all_review'].apply(lambda x: re.sub('[^a-zA-z0-9\s]','',x))
 
-    def lower_case(input_str):
-        input_str = input_str.lower()
-        return input_str
+url = 'https://raw.githubusercontent.com/nicsusuki/first-streamlit-app/main/hotelReviewsInDubai__en2019100120191005.csv'
+df = pd.read_csv(url)
 
-    df_combined['all_review']= df_combined['all_review'].apply(lambda x: lower_case(x))
-    df = df_combined
+df['hotelName'] = df.hotelName.str.split('\n').str[0]
+df['hotelName'] = df.hotelName.str.split('   ').str[1]
+df['hotelName'].drop_duplicates()
+df_combined = df.sort_values(['hotelName']).groupby('hotelName', sort=False).review_body.apply(''.join).reset_index(name='all_review')
+  
+df_combined['all_review'] = df_combined['all_review'].apply(lambda x: re.sub('[^a-zA-z0-9\s]','',x))
 
-    
+def lower_case(input_str):
+    input_str = input_str.lower()
+    return input_str
 
-    
-    
-    return(df)
+df_combined['all_review']= df_combined['all_review'].apply(lambda x: lower_case(x))
+df = df_combined
+
+df_sentences = df.set_index("all_review")
+df_sentences = df_sentences["hotelName"].to_dict()
+df_sentences_list = list(df_sentences.keys())
+
+df_sentences_list = [str(d) for d in tqdm(df_sentences_list)]
+embedder = SentenceTransformer('all-MiniLM-L6-v2')
+# Corpus with example sentences
+corpus = df_sentences_list
+corpus_embeddings = embedder.encode(corpus,show_progress_bar=True)
 
 
 
-def run():
-    st.subheader("Iris Data Loaded into a Pandas Dataframe.")
+# Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
+top_k = min(5, len(corpus))
 
-    df = load_data()
-    
-    df_sentences = df.set_index("all_review")
-    df_sentences = df_sentences["hotelName"].to_dict()
-    df_sentences_list = list(df_sentences.keys())
+query_embedding = embedder.encode(query, convert_to_tensor=True)
 
-    df_sentences_list = [str(d) for d in tqdm(df_sentences_list)]
-    embedder = SentenceTransformer('all-MiniLM-L6-v2')
-    # Corpus with example sentences
-    corpus = df_sentences_list
-    corpus_embeddings = embedder.encode(corpus,show_progress_bar=True)
-    
-    queries = st.text_area("Start typing")
-    
-    # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
-    top_k = min(5, len(corpus))
-    for query in queries:
-        query_embedding = embedder.encode(query, convert_to_tensor=True)
-    
-        # We use cosine-similarity and torch.topk to find the highest 5 scores
-        cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
-        top_results = torch.topk(cos_scores, k=top_k)
-    
-        print("\n\n======================\n\n")
-        print("Query:", query)
-        print("\nTop 5 most similar sentences in corpus:")
+# We use cosine-similarity and torch.topk to find the highest 5 scores
+cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
+top_results = torch.topk(cos_scores, k=top_k)
 
-    for score, idx in zip(top_results[0], top_results[1]):
-        print("(Score: {:.4f})".format(score))
-        print(corpus[idx], "(Score: {:.4f})".format(score))
-        row_dict = df.loc[df['all_review']== corpus[idx]]
-        print("paper_id:  " , row_dict['hotelName'] , "\n")
+    #print("\n\n======================\n\n")
+st.success("Query:", query)
+    #print("\nTop 5 most similar sentences in corpus:")
+
+for score, idx in zip(top_results[0], top_results[1]):
+    st.success("(Score: {:.4f})".format(score))
+    st.success(corpus[idx], "(Score: {:.4f})".format(score))
+    row_dict = df.loc[df['all_review']== corpus[idx]]
+    st.success("paper_id:  " , row_dict['hotelName'] , "\n")
   
     
     
@@ -133,6 +124,8 @@ def run():
 
 
 
-
-if __name__ == '__main__':
-    run()
+# =============================================================================
+# 
+# if __name__ == '__main__':
+#     run()
+# =============================================================================
